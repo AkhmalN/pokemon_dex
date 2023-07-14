9d1e4c02acb3e6c3167fa2311c709b5ecf786712
AkhmalNovanda <cocwar427@gmail.com>
2023-07-14 13:47:23 +0700

    adding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import { Button } from "react-bootstrap"
+import Pokemon from "./components/Pokemon"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   const [allPokemons, setAllPokemons] = useState([])
+   const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+
+   const getAllPokemons = async () => {
+      const res = await fetch(loadMore)
+      const data = await res.json()
+
+      setLoadMore(data.next)
+
+      function pokemonsObject(result) {
+         result.forEach(async (pokemons) => {
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemons.name}`)
+            const data = await res.json()
+            setAllPokemons(currentList => [...currentList, data])
+
+         });
+      }
+
+      pokemonsObject(data.results)
+      await console.log(allPokemons)
+   }
+
+   useEffect(() => {
+      getAllPokemons()
+   }, [])
+   return (
+      <div className="app-container">
+         <h1>Pokemons Evolution</h1>
+         <div className="pokemon-container">
+            <div className="all-container">
+               {allPokemons.map((pokemons, index) => {
+                  return (
+                     <Pokemon
+                        id={pokemons.id}
+                        name={pokemons.name}
+                        image={pokemons.sprites.other.dream_world.front_default}
+                        type={pokemons.types[0].type.name}
+                        key={index}
+                     />
+                  )
+               })}
+            </div>
+         </div>
+         <Button className="load-more" onClick={()=> getAllPokemons()}>Load More</Button>
+      </div>
+   )
 }
 
-export default App;
+export default App
